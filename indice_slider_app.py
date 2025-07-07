b02e5d5818d0c4a635e6841928663978e9871c33
LGoliatt <goliatt@gmail.com>
2025-07-07 20:32:26 -0300

    goliatt seg 07 jul 2025 20:32:26 -03

--- indice_slider_app.py
+++ indice_slider_app.py
@@ -161,11 +161,11 @@ st.markdown("### 🧮 Matriz de Comparação Normalizada")
 matriz_media_ = norm_fuzzy[:, :, 1]
 df_matriz_fuzzy_ = pd.DataFrame(matriz_media_, index=criterios, columns=criterios)
 st.dataframe(df_matriz_fuzzy_, height=250)
-st.dataframe(df_matriz_fuzzy_.sum(axis=1).T)
 
 
 soma_linhas = np.sum(norm_fuzzy, axis=1)
-pesos_defuzzificados = [(l + 2*m + u) / 4. for l, m, u in soma_linhas]
+#pesos_defuzzificados = [(l + 2*m + u) / 4 for l, m, u in soma_linhas]
+pesos_defuzzificados = [(l + 1*m + u) / 3 for l, m, u in soma_linhas]
 pesos_normalizados = pesos_defuzzificados / np.sum(pesos_defuzzificados)
 
 # === PESOS RELATIVOS ===
