1acc1f66b5b37d261cff1d989e9fa6d54c91fb3b
LGoliatt <goliatt@gmail.com>
2025-07-08 06:37:39 -0300

    goliatt ter 08 jul 2025 06:37:39 -03

--- indice_slider_app.py
+++ indice_slider_app.py
@@ -263,7 +263,7 @@ st.dataframe(col_sum_def)
 st.dataframe(pesos_normalizados)
 lambda_max_fuzzy = np.dot(col_sum_def, pesos_normalizados) 
 
-#lambda_max_fuzzy = abs(np.linalg.eigvals(A)).max()
+lambda_max_fuzzy = abs(np.linalg.eigvals(df_matriz_fuzzy)).max()
 
 CI_fuzzy = (lambda_max_fuzzy - n) / (n - 1)
 
