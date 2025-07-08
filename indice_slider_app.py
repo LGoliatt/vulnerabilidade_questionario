b1fa480bdc44974391ef5f4a67fbc1cb69924c84
LGoliatt <goliatt@gmail.com>
2025-07-08 08:18:58 -0300

    goliatt ter 08 jul 2025 08:18:58 -03

--- indice_slider_app.py
+++ indice_slider_app.py
@@ -173,7 +173,7 @@ slider_values = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1
 
 # Definição da escala de importância com rótulos descritivos
 importancia_labels = [
-    "9 - Extrema (à esquerda)",
+    "9 - Extrema (à esquerda)", 
     "8 - Entre muito forte à extrema (à esquerda)",
     "7 - Muito forte (à esquerda)",
     "6 - Entre forte à muito forte (à esquerda)",
