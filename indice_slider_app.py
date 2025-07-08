32bff73ed8a7324fb69c56f07e10fda81358bcfa
LGoliatt <goliatt@gmail.com>
2025-07-07 22:12:59 -0300

    goliatt seg 07 jul 2025 22:12:58 -03

--- indice_slider_app.py
+++ indice_slider_app.py
@@ -260,7 +260,8 @@ st.markdown("### 📈 Métricas de Consistência (Estimadas para FAHP)")
 
 col_sum_def = np.sum(matriz_crisp, axis=0)  # Usa valores médios
 st.dataframe(col_sum_def)
-lambda_max_fuzzy = np.dot(col_sum_def, pesos_normalizados/pesos_normalizados) 
+st.dataframe(pesos_normalizados)
+lambda_max_fuzzy = np.dot(col_sum_def, pesos_normalizados) 
 
 CI_fuzzy = (lambda_max_fuzzy - n) / (n - 1)
 
